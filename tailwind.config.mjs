48c55a9efeb19e898343047d71cb418753dc2cc6
Javier Carrasco <javierdeveloper08@gmail.com>
2024-02-25 12:30:58 -0400

    Change css variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        body: '#293241',
+        color: '#FFFFFF',
+        particles: '#8338EC',
+        card: '#3D405B',
+        footer: '#0D1B2A',
+        header: '#1b263b',
+        icon: '#E5E5E5',
+      },
+      maxWidth: {
+        desktop: '1400px',
+      },
+      fontFamily: {
+        raleway: ['Raleway Variable', ...defaultTheme.fontFamily.sans],
+        montserrat: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
